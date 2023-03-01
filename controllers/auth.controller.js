19ace6159299f9f45f75e24f87758b73cf57af44
manish <manish.prajapati.se@gmail.com>
2023-03-01 11:26:08 +0530

    added auth views and styled them

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,11 +2,17 @@ function getSignup(req,res){
     res.render('auth/signup');
 }
 
+function signup(req,res){
+}
+
 function getLogin(req,res){
     res.render('auth/login');
 }
 
+
+
 module.exports={
     getSignup:getSignup,
-    getLogin:getLogin
+    getLogin:getLogin,
+    signup:signup
 }
